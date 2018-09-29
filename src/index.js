702fd616bf3f3855a073771596c9cb6f4749e19f
eve <ewashadevsk@gmail.com>
2018-09-29 12:00:12 +0300

    sulution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const coins = [ 50, 25, 10, 5, 1 ];
+    const labels = [ 'H', 'Q', 'D', 'N', 'P' ];
+    const result = {};
+
+    if (currency > 10000) {
+        return { 
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        };
+    }
+
+    let currentCoinIndex = 0;
+    while (currency > 0) {
+        let remainder = currency - coins[currentCoinIndex];
+        if (remainder >= 0) {
+            currency = remainder;
+            
+            // remember coin
+            if (result[labels[currentCoinIndex]]) {
+                ++result[labels[currentCoinIndex]];
+            } else {
+                result[labels[currentCoinIndex]] = 1;
+            }
+        } else {
+            ++currentCoinIndex;
+        }
+    }
+
+    return result;
 }
